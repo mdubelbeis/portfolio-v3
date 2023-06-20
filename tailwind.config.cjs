b5251387854ef039768aeb6d3a1f1870ff0829d0
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2023-06-20 12:04:51 -0500

    reconfigured tailwind config file for color corrections

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,24 +4,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        darkPurple: "#1C0024",
-        textPurple: "#580071",
-        deepPlum: "#321939",
-        royalPurple: "#49324f",
-        amethyst: "#604c65",
-        smokeyLavender: "#76667b",
-        dustyLilac: "#8d7f91",
-        silverPlum: "#a499a7",
-        mauveMist: "#bab2bd",
-        pearlGray: "#d1ccd3",
         paleLilac: "#e8e5e9",
-
-        primaryPink: "#DB2877",
-        dullPinkLighter: "#F04FA0",
-        lightPink: "#F46FB2",
-        primaryPinkLightest: "#F8A1C9",
-        primaryPinkLightest: "#FBC3E0",
-        primaryPinkLightest: "#FDE5F7",
+        secondaryText: "#e8e5e9",
+        primaryBg: "#172454",
+        primaryText: "#446879",
+        buttonBg: "#22C55D",
+        buttonBgHover: "#1D9E4F",
       },
       fontFamily: {
         abril: ["Abril Fatface", "cursive"],
